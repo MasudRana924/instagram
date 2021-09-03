a09ac6073fa448656f3885cff653a8bc80669d31
MasudRana924 <masud15-924@diu.edu.bd>
2021-09-03 07:10:44 -0600

    login added

--- signup.js
+++ signup.js
@@ -1,13 +1,18 @@
-document.getElementById('login-button').addEventListener('click', function () {
+document.getElementById('signup-button').addEventListener('click', function () {
     const email = document.getElementById('email')
     const userEmail = email.value;
+    const firstName = document.getElementById('f-name')
+    const userFname = firstName.value
+    const lastName = document.getElementById('l-name')
+    const userLname = lastName.value
     const pass = document.getElementById('pass')
     const password = pass.value
     // user pass
     const minLength = 1
     const maxLength = 30
     if (userEmail.length >= minLength ||userEmail.length <= maxLength 
-     
+        && userFname >= minLength  || userFname <= maxLength
+         && userLname >= minLength || userLname <= maxLength
           && password >= minLength ||password <= maxLength  ) {
         window.location.href = 'insta.html'
     }
@@ -15,3 +20,4 @@ document.getElementById('login-button').addEventListener('click', function () {
         console.log('wrong')
     }
 })
+

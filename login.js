c3252fbbbdb0211bcd2c1d91b904b90c323cc5bb
MasudRana924 <masud15-924@diu.edu.bd>
2021-09-03 06:14:09 -0600

    login edited

---
+++ login.js
@@ -0,0 +1,22 @@
+document.getElementById('login-button').addEventListener('click', function () {
+    const email = document.getElementById('email')
+    const userEmail = email.value;
+    const firstName = document.getElementById('f-name')
+    const userFname = firstName.value
+    const lastName = document.getElementById('l-name')
+    const userLname = lastName.value
+    const pass = document.getElementById('pass')
+    const password = pass.value
+    // user pass
+    const minLength = 1
+    const maxLength = 30
+    if (userEmail.length >= minLength ||userEmail.length <= maxLength 
+        && userFname >= minLength  || userFname <= maxLength
+         && userLname >= minLength || userLname <= maxLength
+          && password >= minLength ||password <= maxLength  ) {
+        window.location.href = 'insta.html'
+    }
+    else{
+        console.log('wrong')
+    }
+})
